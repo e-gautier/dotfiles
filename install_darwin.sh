7740700692b12a6d6442bafe1fc4f5e1254b16e8
Etienne G <etienne.gautier@protonmail.com>
2022-01-09 13:00:51 +0100

    cleaning bash scripts

--- install_darwin.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-set -e
-
-# binaries
-awk '/--- '$INSTALL_MODE'/{n=1;next}/---/{n=0}{if(n) system("sudo brew install "$0)}' binaries
--- install_nftables.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-sudo cp ${BASEDIR}/nftables/inet-filter.nft /etc/nftables
-sudo chmod +x /etc/nftables/inet-filter.nft
-sudo sed -i 's|# include "/etc/nftables/inet-filter.nft"|include "/etc/nftables/inet-filter.nft"|g' /etc/sysconfig/nftables.conf
-sudo systemctl enable --now nftables
